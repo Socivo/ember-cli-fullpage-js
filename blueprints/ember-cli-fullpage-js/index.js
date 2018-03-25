baed5b323555290da0993daf3e9715b47bbb3be5
Sanket Sharma <sanket.sharma@absa.co.za>
2018-03-25 16:24:35 +0200

    Initial import

---
+++ blueprints/ember-cli-fullpage-js/index.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+module.exports = {
+  description: 'Exports fullpage.js library',
+
+  // locals(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall(options) {
+  //   // Perform extra work here.
+  // }
+  afterInstall() {
+    return this.addPackageToProject('fullpage.js');
+  }
+};
+
